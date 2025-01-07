805343d5881e52d41b9c923acc46ad8943a6c06f
Rasibul <rasibul179@gmail.com>
2025-01-07 21:59:09 +0600

    create a schema file for user

---
+++ src/modules/Login/login.schema.js
@@ -0,0 +1,22 @@
+const { Schema } = require("mongoose");
+
+const loginSchema = new Schema({
+
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+    },
+    password: { type: String, required: [true, "Password is required"] },
+    role: {
+        type: String,
+        enum: ["admin", "user"],
+        default: "user",
+    },
+
+}, { timestamps: true })
+
+
+const MLogin = mongoose.model("User", loginSchema);
+
+module.exports = MLogin;
